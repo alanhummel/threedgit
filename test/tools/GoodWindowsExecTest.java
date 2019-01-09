3cacf913c724bd0acbc772b081eba26f02175c79
Alan Hummel <ahummel@tarion.com>
2019-01-09 16:01:55 -0500

    Improved test that works with Linux and Windows.

--- test/tools/GoodWindowsExecTest.java
+++ test/tools/GoodWindowsExecTest.java
@@ -6,7 +6,13 @@ public class GoodWindowsExecTest {
 
     @Test
     public void testExecute() {
-        GoodWindowsExec.execute("ls -al");
-        GoodWindowsExec.execute("cd ~/Projects/C\\ and\\ C++/DataStructures/ && git diff");
+        String osName = System.getProperty("os.name" );
+        if( osName.startsWith( "Windows" ) ) {
+            GoodWindowsExec.execute("dir /s");
+            GoodWindowsExec.execute("cd C:\\Projects\\threedgit && git diff");
+        } else if (osName.equals("Linux")) {
+            GoodWindowsExec.execute("ls -al");
+            GoodWindowsExec.execute("cd ~/Projects/C\\ and\\ C++/DataStructures/ && git diff");
+        }
     }
 }
